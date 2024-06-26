fe6bb485931a32dd434be3436d401aeab6b79300
Tayyaba Khalil <tayyaba.khalil@gmail.com>
2024-06-26 16:25:59 +0300

    changes in Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="my-12"
+    <div
+      className="my-12"
       style={{
         backgroundImage: "url('/assets/footer.svg')",
         backgroundSize: "100%",
-        backgroundRepeat: "no-repeat",  
+        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="max-w-7xl flex ml-[92px]">
@@ -17,7 +18,9 @@ const Footer = () => {
               alt="Lunex Logo"
               className="mb-4 w-32"
             />
-            <span className="text-[60px] text-white font-bold pl-8 pt-4">LUNEX</span>
+            <span className="text-[60px] text-white font-bold pl-8 pt-4">
+              LUNEX
+            </span>
           </div>
 
           <button className="neon-border neon-shadow neon-hover mx-12 px-14 py-2 ml-12 mt-10 bg-black font-bold flex items-center transition-colors duration-300 ease-in-out">
@@ -42,18 +45,24 @@ const Footer = () => {
                 <p className="text-small font-bold text-[#34fcb4]">
                   Technical Support:
                 </p>
-                <p className="text-white text-[12px]">support@lunexnetwork.io</p>
+                <p className="text-white text-[12px]">
+                  support@lunexnetwork.io
+                </p>
               </div>
             </div>
-            <div className="flex items-start  space-x-2 ml-14" >
+            <div className="flex items-start  space-x-2 ml-14">
               <img
                 src="/assets/email-icon.svg"
                 alt="Email"
                 className="h-5 mb-5"
               />
               <div className="flex flex-col ">
-                <p className="text-small font-bold text-[#34fcb4]">Marketing:</p>
-                <p className="text-white text-[12px]">marketing@lunexnetwork.io</p>
+                <p className="text-small font-bold text-[#34fcb4]">
+                  Marketing:
+                </p>
+                <p className="text-white text-[12px]">
+                  marketing@lunexnetwork.io
+                </p>
               </div>
             </div>
             <div className="flex items-start space-x-2 ml-12">
@@ -63,13 +72,17 @@ const Footer = () => {
                 className="h-5 mb-5"
               />
               <div className="flex flex-col">
-                <p className="text-small font-bold text-[#34fcb4]">Investors:</p>
-                <p className="text-white text-[12px]">investors@lunexnetwork.io</p>
+                <p className="text-small font-bold text-[#34fcb4]">
+                  Investors:
+                </p>
+                <p className="text-white text-[12px]">
+                  investors@lunexnetwork.io
+                </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="absolute flex flex-row ml-[710px] mt-[140px]">
+        <div className="absolute flex flex-row ml-[710px] mt-[205px]">
           <p className=" [font-family:'Archivo-Medium',Helvetica] font-medium text-transparent text-lg  whitespace-nowrap">
             <span className="text-[#34fcb4]">
               {String.fromCharCode(47, 47)}
@@ -101,18 +114,18 @@ const Footer = () => {
           </href>
         </div>
       </div>
-      <div className="max-w-7xl flex flex-row items-start text-gray-500 text-sm">
-        <p className="flex-1 mb-4 md:mb-0 mt-8 max-w-[650px] ml-28">
+      <div className="max-w-7xl flex flex-row text-gray-500 text-sm">
+        <p className="flex-1 mb-4 md:mb-0 mt-8 max-w-[650px] ml-36">
           Disclaimer: Digital currencies may be unregulated in your
           jurisdiction. The value of digital currencies may go down as well as
           up. Profits may be subject to capital gains or other taxes applicable
           in your jurisdiction.
         </p>
-        <div className="flex flex-row ">
-          <a href="#" className="hover:text-white mt-8  mb-4 md:mb-0 ml-12">
+        <div className="flex flex-row">
+          <a href="#" className="hover:text-white mt-8 mb-4 md:mb-0 ml-8">
             Privacy
           </a>
-          <a href="#" className="hover:text-white mt-8  mb-4 md:mb-0 ml-20">
+          <a href="#" className="hover:text-white mt-8 mb-4 md:mb-0 ml-44">
             Terms & Conditions
           </a>
         </div>
