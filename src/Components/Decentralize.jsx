1a10ca135d72d490bbd97b54b874fa6cdefa261c
faria2001 <fariaejaz2001@gmail.com>
2024-06-26 23:10:27 +0500

    decentralized gradient added

--- src/Components/Decentralize.jsx
+++ src/Components/Decentralize.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Decentralize = () => {
   return (
     <div
-      className="relative bg-black flex justify-center border border-browngrey border-[0.25px]"
+      className="relative bg-black flex justify-center border-browngrey border-[0.25px]"
       style={{
         background:
-          "radial-gradient(circle 50vh at 10% 5%, #273b1d, transparent)",
+          "radial-gradient(circle 50vh at 60% 75%, #273b1d, transparent)",
       }}
     >
       <div
